042b9d753b3147a538cd8757e95b3923511eae03
7286065491 <41770153+7286065491@users.noreply.github.com>
2018-08-30 12:07:40 +0530

    Update even number.py

--- even number.py
+++ even number.py
@@ -2,4 +2,4 @@ n1,n2=map(int,input().spilt())
 for i in range(n1+1,n2,1):
                 k=i%2
                 if(k==0):
-                         print(i,end="")
+                         print(i,end=" ")
