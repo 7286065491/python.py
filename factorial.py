cf0d6d6193bcd88a7a5644de81c5cdfba186d1a2
7286065491 <41770153+7286065491@users.noreply.github.com>
2018-09-01 12:19:28 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,9 @@
+x=int(input())
+fact=1
+if x>o:
+       for i in range(1,x+1):
+                      fact=fact*i
+               print(fact)
+else:
+               print("invalid")
+                      
